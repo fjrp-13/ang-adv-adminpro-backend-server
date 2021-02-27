715fc7e1027927ce9824ff1a2fdec8135b86e6db
Fran <rivilla13@gmail.com>
2021-02-27 19:57:59 +0100

    Fixed bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,8 +36,8 @@ const login = async (req, res = response) => {
         // Generar el token JWT (usamos .id y Mongoose ya sabrá intrepretar que buscamos el ID del usuario)
         const token = await generarJWT(usuarioDB.id);
 
-        return res.status(400).json({
-            success: false,
+        return res.status(200).json({
+            success: true,
             token
         });
 
